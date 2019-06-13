e7c85d4b4c29fe75d8ff0b4d41c7bb96fe540f46
Jovan <abc.j.jovan@gmail.com>
2019-06-13 20:32:37 +0200

    Minor update

--- Arduino-RF-4CH.ino
+++ Arduino-RF-4CH.ino
@@ -12,87 +12,87 @@ int ledPin4 = 6; //Relej D
   A3 = B
   A0 = C
   A2 = D
- 
- */
+
+*/
 
 void setup() {
-pinMode(A0, INPUT);
-pinMode(A1, INPUT);
-pinMode(A2, INPUT);
-pinMode(A3, INPUT);
-pinMode(ledPin1, OUTPUT);
-pinMode(ledPin2, OUTPUT);
-pinMode(ledPin3, OUTPUT);
-pinMode(ledPin4, OUTPUT);
+  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
+  pinMode(A2, INPUT);
+  pinMode(A3, INPUT);
+  pinMode(ledPin1, OUTPUT);
+  pinMode(ledPin2, OUTPUT);
+  pinMode(ledPin3, OUTPUT);
+  pinMode(ledPin4, OUTPUT);
 }
 
 void loop() {
-  lampa ();
-  kompjuter();
-  ledTraka();
-  ugasiSveDelay();
- }
+  lampa (); // Ukljuci stone lampe
+  kompjuter(); // Ukljuci/iskljuci kompjuter
+  ledTraka(); // Ukljuci led trake
+  ugasiSveDelay(); // Iskljuci sve nakon zadatog delay-a
+}
 
 void lampa ()
 {
- // Dugme A na prekidaču aktivira pin A1
- if(digitalRead(A1)) 
-  digitalWrite(ledPin1, HIGH);
- 
- // Dugme B na prekidaču deaktivira pin A1
- if(digitalRead(A3))
-  digitalWrite(ledPin1, LOW); 
+  // Dugme A na prekidaču aktivira pin A1
+  if (digitalRead(A1))
+    digitalWrite(ledPin1, HIGH);
+
+  // Dugme B na prekidaču deaktivira pin A1
+  if (digitalRead(A3))
+    digitalWrite(ledPin1, LOW);
 }
 
-void kompjuter() 
+void kompjuter()
 {
   // Dugme D na prekidaču aktivira A2 i deaktivira nakon 100 ms.
-  //Svrha je ukljucivanje kompjutera koji zahteva kratak kontakt, a ne konstantan kontakt
-  if(digitalRead(A2))  
-  digitalWrite(ledPin2, HIGH);
+  // Svrha je ukljucivanje kompjutera koji zahteva kratak kontakt, a ne konstantan kontakt
+  if (digitalRead(A2))
+    digitalWrite(ledPin2, HIGH);
   delay (100);
   digitalWrite(ledPin2, LOW);
 }
 
-void ledTraka(){
+void ledTraka() {
   // Dugme C na prekidaču aktivira pin A0
-  if(digitalRead(A0))
-  digitalWrite(ledPin3, HIGH);
+  if (digitalRead(A0))
+    digitalWrite(ledPin3, HIGH);
 
-  if(digitalRead(A3))
-  digitalWrite(ledPin3, LOW);
+  if (digitalRead(A3))
+    digitalWrite(ledPin3, LOW);
 }
 
-void ugasiSveDelay(){
-    //Ova metoda iskljucuje svetla u kuci nakon odredjenog vremena
-    //Nakon pritiska C i B na prekidacu
-    //Aktivira se relej povezan na ledPin3
-    //Koji ce se okinuti 7 puta, kako bi oznacio pocetak rada
-  
-    if((digitalRead(A0)) && (digitalRead(A3))){
+void ugasiSveDelay() {
+  // Ova metoda iskljucuje svetla u kuci nakon odredjenog vremena
+  // Nakon pritiska C i B na prekidacu
+  // Aktivira se relej povezan na ledPin3
+  // Koji ce se okinuti 7 puta, kako bi oznacio pocetak rada
+
+  if ((digitalRead(A0)) && (digitalRead(A3))) {
     int a = 0;
-    while( a<=7){
-    digitalWrite(ledPin3, HIGH);
-    delay(1000);
-    digitalWrite(ledPin3, LOW);
-    delay(1000);
-    a++;
+    while ( a <= 7) {
+      digitalWrite(ledPin3, HIGH);
+      delay(1000);
+      digitalWrite(ledPin3, LOW);
+      delay(1000);
+      a++;
     };
 
-    //Posle 7 okidanja releja
-    //Ukljucice se led traka i lampe
-    //Koje ce ostati ukljucene koliko je navedeno u kodu ispod
-    
+    // Posle 7 okidanja releja
+    // Ukljucice se led traka i lampe
+    // Koje ce ostati ukljucene koliko je navedeno u kodu ispod
+
     digitalWrite(ledPin3, HIGH);
     digitalWrite(ledPin1, HIGH);
     int cekanje = 60; // Posle koliko sekundi da se sva svetla iskljuce
     int i = 0;
-    while(i <= cekanje){
-    delay(1000);
-    i++;
+    while (i <= cekanje) {
+      delay(1000);
+      i++;
     }
-    //Potom ce se sve iskljuciti
+    // Potom ce se sve iskljuciti
     digitalWrite(ledPin3, LOW);
     digitalWrite(ledPin1, LOW);
-    }
+  }
 }
